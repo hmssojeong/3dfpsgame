ed1ab6b4bf461600fac065729be07a962e1559c0
hmssojeong <sojeong9638@gmail.com>
2025-12-08 16:29:31 +0900

    feat: 캐릭터 컨트롤러를 이용한 이동

--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -1,29 +1,39 @@
 using UnityEngine;
 
-//키보드를 누르면 캐릭터를 그 방향으로 이동 시키고 싶다.
+// 키보드를 누르면 캐릭터를 그 방향으로 이동 시키고 싶다.
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
     // 필요 속성
     // - 이동속도
-    public float moveSpeed = 7f;
+    public float MoveSpeed = 7;
+
+    private CharacterController _controller;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
 
     private void Update()
     {
         // 1. 키보드 입력 받기
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
-        // 2. 입력에 따른 방향 구하기
-        // 현재는 유니티 세상의 절대적인 방향 (글로벌 좌표계)
+
+        // 2. 입력에 따른 방향 구하기 
+        // 현재는 유니티 세상의 절대적인 방향이 기준 (글로벌/월드 좌표계)
         // 내가 원하는 것은 카메라가 쳐다보는 방향이 기준으로
 
-        // - 글로벌 좌표 방향을 구한다.
-        Vector3 direction = new Vector3(x, 0, y); // 3d에서 중간에 0 자리는 점프를 말한다
+        // - 글로벌 좌표 방향을 구한다. 
+        Vector3 direction = new Vector3(x, 0, y);
         direction.Normalize();
-        // - 카메라가 쳐다보는 방향으로 변환한다. (월드->로컬)
+        // - 카메라가 쳐다보는 방향으로 변환한다. (월드 -> 로컬)
         direction = Camera.main.transform.TransformDirection(direction);
 
-        // 3. 방향으로 이동시키기
-        transform.position += direction * moveSpeed * Time.deltaTime;
+
+        // 3. 방향으로 이동시키기  
+        _controller.Move(direction * MoveSpeed * Time.deltaTime);
     }
 
 }
