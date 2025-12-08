6a0307b81d90778ff285eed8cd39ae72e4082950
hmssojeong <sojeong9638@gmail.com>
2025-12-08 14:47:07 +0900

    feat: 마우스 입력 받기

---
+++ Assets/02.Scripts/Camera/CameraRotate.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+// 카메라 회전 기능
+// 마우스를 조작하면 카메라를 그 방향으로 회전하고 싶다.
+public class CameraRotate : MonoBehaviour
+{
+    private void Update()
+    {
+        // 1. 마우스 입력 받기
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        Debug.Log($"{mouseX}, {mouseY}");
+
+        // 2. 입력에 따른 회전 방향 만들기
+        // 3. 회전방향으로 카메라 회전하기
+    }
+
+}
