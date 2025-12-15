fc75d2140f5944ef3569d9e422bab630470411b6
hmssojeong <sojeong9638@gmail.com>
2025-12-15 18:52:51 +0900

    feat: 미니맵 확대 축소

--- Assets/02.Scripts/CursorManager.cs
+++ Assets/02.Scripts/CursorManager.cs
@@ -2,20 +2,38 @@ using UnityEngine;
 
 public class CursorManager : MonoBehaviour
 {
+    private bool isLockCursor = true;
+
     void Awake()
     {
         LockCursor();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isLockCursor)
+            {
+                UnlockCursor();
+            }
+            else
+            {
+                LockCursor();
+            }
+        }
+    }
     public void LockCursor()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        isLockCursor = true;
     }
 
     public void UnlockCursor()
     {
         Cursor.lockState = CursorLockMode.None;
         Cursor.visible = true;
+        isLockCursor = false;
     }
 }
