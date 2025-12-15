eb9653ce597db38df7e478d71ae5fa85d9b362d8
hmssojeong <sojeong9638@gmail.com>
2025-12-15 20:08:29 +0900

    feat: 몬스터 피격시 게이지 딜레이

--- Assets/02.Scripts/Monster/MonsterHealthBar.cs
+++ Assets/02.Scripts/Monster/MonsterHealthBar.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,7 +7,9 @@ public class MonsterHealthBar : MonoBehaviour
 {
     private Monster _monster;
     [SerializeField] private Transform _healthBarTransform;
+    [SerializeField] private Image _guageImageDelay;
     [SerializeField] private Image _guageImage;
+    [SerializeField] private Image _guageImageDelayLate;
 
     private float _lastHealth = -1;
 
@@ -23,10 +26,24 @@ public class MonsterHealthBar : MonoBehaviour
         {
             _lastHealth = _monster.Health.Value;
             _guageImage.fillAmount = _monster.Health.Value / _monster.Health.MaxValue;
+            StartCoroutine(HitDelayGuage_Coroutine());
+            StartCoroutine(HitDelayLateGuage_Coroutine());
         }
 
         // 빌보드 기법: 카메라의 위치와 회전에 상관없이 항상 정면을 바라보게하는 기법
         _healthBarTransform.forward = Camera.main.transform.forward;
 
+    }
+
+    private IEnumerator HitDelayGuage_Coroutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+        _guageImageDelay.fillAmount = _monster.Health.Value / _monster.Health.MaxValue;
+    }
+
+    private IEnumerator HitDelayLateGuage_Coroutine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        _guageImageDelayLate.fillAmount = _monster.Health.Value / _monster.Health.MaxValue;
     }
 }
