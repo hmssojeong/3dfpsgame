6f40457abc68fd9d0f1ef43c36467d036d1548b7
hmssojeong <sojeong9638@gmail.com>
2025-12-19 12:21:24 +0900

    chore: 플레이어 움직임 모션수정

--- Assets/02.Scripts/Player/PlayerGunFire.cs
+++ Assets/02.Scripts/Player/PlayerGunFire.cs
@@ -21,11 +21,16 @@ public class PlayerGunFire : MonoBehaviour
     [Header("반동")]
     [SerializeField] private FPSTPSCameraController _cameraController;
 
+    [Header("총알 궤적 설정")]
+    [SerializeField] private LineRenderer _bulletTrailPrefab;
+    [SerializeField] private float _bulletSpeed = 100f;
+    [SerializeField] private float _trailDuration = 0.1f;
+
     private float _fireTimer = 0f; // 남은 쿨타임
 
     private void Start()
     {
-        if(_cameraController == null)
+        if (_cameraController == null)
         {
             _cameraController = FindAnyObjectByType<FPSTPSCameraController>();
         }
@@ -56,7 +61,7 @@ public class PlayerGunFire : MonoBehaviour
         {
             _animator.SetBool("Fire", false);
         }
-       
+
     }
 
     private IEnumerator MuzzleFlash_Coroutine()
@@ -84,58 +89,105 @@ public class PlayerGunFire : MonoBehaviour
         }
     }
 
-        private void Fire()
+    private void Fire()
+    {
+        // 반동
+        if (_cameraController != null)
         {
-            // 반동
-            if (_cameraController != null)
+            _cameraController.ApplyGunRebound();
+        }
+
+        // 2. Ray를 생성하고 발사할 위치, 방향, 거리를 설정한다. (쏜다.)
+        /*Ray ray = new Ray(_fireTransform.position, Camera.main.transform.forward);*/
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        // 3. RayCastHit(충돌한 대상의 정보)를 저장할 변수를 생성한다.
+        RaycastHit hitInfo = new RaycastHit();
+
+        Vector3 startPos = _fireTransform.position;
+        Vector3 endPos;
+
+        // 4. 발사하고
+        bool isHit = Physics.Raycast(ray, out hitInfo);
+        if (isHit)
+        {
+            // 5. 충돌했다면... 피격 이펙트 표시
+            Debug.Log(hitInfo.transform.name);
+
+            // 파티클 생성과 플레이 방식
+            // 1. Instantiate 방식 (+ 풀링) -> 한 화면에 여러가지 수정 후 여러개 그릴경우
+            // 2. 하나를 캐싱해두고 Play    -> 인스펙터 설정 그대로 그릴 경우 // 한 화면에 한번만 그릴 경우
+            // 3. 하나를 캐싱해두고 Emit    -> 한 화면에 위치만 수정 후 여러개 그릴 경우 ->인스펙터 속성을 바꾸고싶다면 Emit쓰기 ex) END색깔을 바꾼다던지 할 때
+
+            //_hitEffect.transform.position = hitInfo.point;
+            //_hitEffect.transform.forward = hitInfo.normal;
+
+            endPos = hitInfo.point;
+
+            _hitEffect.transform.position = hitInfo.point;
+            _hitEffect.transform.forward = hitInfo.normal;
+
+            _hitEffect.Play();
+
+            Monster monster = hitInfo.collider.gameObject.GetComponent<Monster>();
+            if (monster != null)
             {
-                 _cameraController.ApplyGunRebound();
+                monster.TryTakeDamage(_playerAttackDamage, transform.position);
             }
 
-            // 2. Ray를 생성하고 발사할 위치, 방향, 거리를 설정한다. (쏜다.)
-            /*Ray ray = new Ray(_fireTransform.position, Camera.main.transform.forward);*/
-             Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
-
-            // 3. RayCastHit(충돌한 대상의 정보)를 저장할 변수를 생성한다.
-            RaycastHit hitInfo = new RaycastHit();
-
-            // 4. 발사하고
-            bool isHit = Physics.Raycast(ray, out hitInfo);
-            if (isHit)
+            Drum drum = hitInfo.collider.gameObject.GetComponent<Drum>();
+            if (drum != null)
             {
-                // 5. 충돌했다면... 피격 이펙트 표시
-                Debug.Log(hitInfo.transform.name);
+                drum.TryTakeDamage(10);
+            }
 
-                // 파티클 생성과 플레이 방식
-                // 1. Instantiate 방식 (+ 풀링) -> 한 화면에 여러가지 수정 후 여러개 그릴경우
-                // 2. 하나를 캐싱해두고 Play    -> 인스펙터 설정 그대로 그릴 경우 // 한 화면에 한번만 그릴 경우
-                // 3. 하나를 캐싱해두고 Emit    -> 한 화면에 위치만 수정 후 여러개 그릴 경우 ->인스펙터 속성을 바꾸고싶다면 Emit쓰기 ex) END색깔을 바꾼다던지 할 때
-
-                //_hitEffect.transform.position = hitInfo.point;
-                //_hitEffect.transform.forward = hitInfo.normal;
-
-                _hitEffect.transform.position = hitInfo.point;
-                _hitEffect.transform.forward = hitInfo.normal;
-
-                _hitEffect.Play();
-
-                Monster monster = hitInfo.collider.gameObject.GetComponent<Monster>();
-                if(monster != null)
-                {
-                     monster.TryTakeDamage(_playerAttackDamage, transform.position);
-                }
-
-                Drum drum = hitInfo.collider.gameObject.GetComponent<Drum>();
-                if (drum != null)
-                {
-                    drum.TryTakeDamage(10);
-                }
+            if (_bulletTrailPrefab != null)
+            {
+                StartCoroutine(DrawBulletTrail(startPos, endPos));
+            }
+            else
+            {
+                endPos = ray.GetPoint(100f);
+            }
 
         }
+    }
+
+    private IEnumerator DrawBulletTrail(Vector3 startPos, Vector3 endPos)
+    {
+        LineRenderer trail = Instantiate(_bulletTrailPrefab);
+        trail.positionCount = 2;
+        trail.SetPosition(0, startPos);
+        trail.SetPosition(1, endPos);
+
+        float distance = Vector3.Distance(startPos, endPos);
+        float travelTime = distance / _bulletSpeed;
+        float elapsedTime = 0f;
+
+        // 총알이 날아가는 애니메이션
+        while (elapsedTime < travelTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / travelTime;
+
+            Vector3 currentPos = Vector3.Lerp(startPos, endPos, t);
+            trail.SetPosition(1, currentPos);
+
+            yield return null;
         }
+
+        // 최종 위치 설정
+        trail.SetPosition(1, endPos);
+
+        // 궤적 페이드 아웃
+        yield return new WaitForSeconds(_trailDuration);
+
+        Destroy(trail.gameObject);
+    }
+}
+ 
 
         // Ray: 레이저(시작위치, 방향, 거리)
         // RayCast: 레이저를 발사
         // RayCastHit: 레이저가 물체와 충돌했다면 그 정보를 저장하는 구조체
 
- }
